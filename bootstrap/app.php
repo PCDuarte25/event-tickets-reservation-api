9302f88a7be9176c310f6d13b958196c59c6c097
Pedro Caio <pedroduarte@chuva-inc.com>
2025-02-02 23:35:20 -0300

    feat: Create exceptions to handle with api errors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Exceptions\NotEnoughTicketsException;
+use App\Exceptions\ReservationNotInEventException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +21,24 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (NotEnoughTicketsException $e, $request) {
+            return response()->json(['error' => $e->getMessage()], Response::HTTP_UNPROCESSABLE_ENTITY);
+        });
+
+        $exceptions->renderable(function (ReservationNotInEventException $e, $request) {
+            return response()->json(['error' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+        });
+
+        $exceptions->renderable(function (ModelNotFoundException $e, $request) {
+            return response()->json(['error' => $e->getMessage()], Response::HTTP_NOT_FOUND);
+        });
+
+        $exceptions->renderable(function (NotFoundHttpException $e, $request) {
+            return response()->json(['error' => $e->getMessage()], Response::HTTP_NOT_FOUND);
+        });
+
+        $exceptions->renderable(function (QueryException $e, $request) {
+            $errorMessage = "An error occurred while processing your request, SQL error: {$e->getMessage()}";
+            return response()->json(['error' => $errorMessage], 400);
+        });
     })->create();
